a6ecfcfa932c1e16bc235b03eeb6e5b2a10474fe
stw32 <63592847+stw32@users.noreply.github.com>
2020-04-13 21:52:28 +0100

    Update Rscript.R

--- Rscript.R
+++ Rscript.R
@@ -29,10 +29,43 @@ Draw<-function()
 
 }
 
+#Downsample
+# The BioRadio device provides 250 samples per second, but the PPG is only sampled 75 times per second, 
+# so we typically have repeated values in a pattern of 3-3-4 repeating. DownSample tries to retrieve the 
+# unique values, with an effort to be robust against variation in the repeat pattern and also against 
+# genuine repeated values.
+
+DownSample <- function(input)   #Clarify TraceCount and SampleModulo
+{
+  
+}
+
+#Undetrend
+# Analysis of device output indicates that the PPG signal is detrended by application of the following
+# formula: OUT[i] = 80 + (OUT[i-1]-80) * 0.96875 + (IN[i] - [IN[i-1]), where the constance 0.96875 is
+# an approximation fitted to the data.
+# Individual pulse events are more comprehensible if the detrending is not used, so this function 
+# removes it by inverting the above function. 
+
+Undetrend <- function(Input, PrevInput, PrevOutput)
+{
+  
+}
+
+#VerifySignal
+# Detect when the input signal indicates device removal.  If the un-detrended data is a constant,
+# the PPG device has probably been removed.
+
+VerifySignal <- function()
+{
+  
+}
+
+
 ###more functions...
 
 
-#The BioRadio device provides 250 samples per second, but the PPG is only sampled 75 times per second, so we typically have repeated values in a pattern of 3-3-4 repeating.  DownSample tries to retrieve the unique values, with an effort to be robust against variation in the repeat pattern and also against genuine repeated values.
+
 list<-rle(data$PPG.PulseOx1)
 realrepeats<-rep(list$lengths > 4,times = list$lengths)
 
