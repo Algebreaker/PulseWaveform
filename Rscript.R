aa8edfd46571a9c335766710a6bbcc6be2065b4f
stw32 <63592847+stw32@users.noreply.github.com>
2020-04-13 15:07:07 +0100

    Update Rscript.R

--- Rscript.R
+++ Rscript.R
@@ -2,6 +2,17 @@ setwd("/home/johanna/Documents/Ninja theory/PulseAnalysis/Data/Craig")
 data <- read.table("Source.csv", header=T, sep=",") #first line of the csv file needs to be deleted
 library()
 
+min_beats_per_minute <- 30
+max_beats_per_minute <- 240
+min_interval <- 0.25
+max_interval <- 4
+min_amplitude <- 0.5
+min_frac_amplitude <- 0.2
+sample_rate <- 75
+peak_interval <- 9
+peak_width <- 2.7   # Should peak width be an array of differing widths per peak?
+skipped_beat_fraction <- 1.75
+
 #main function
 FNTBioRadioPulse<-function(input)
 {
