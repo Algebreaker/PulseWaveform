66792cd82e39145471fef7b825bd94e7f8d49a9f
lkdw <57353460+lkdw@users.noreply.github.com>
2020-06-25 13:02:56 +0100

    Update preproc.R
    Added baseline correction here

--- preproc.R
+++ preproc.R
@@ -10,7 +10,7 @@ preproc <- function(dat){
   
   list<-rle(dat$PPG.PulseOx1)
   ID <- rep(1:length(list$values), times = list$lengths)
-  data2 <- cbind(dat, ID)
+  data2 <- cbind(data, ID)
   data_downsampled <-c()
   
   for (i in 1:max(ID)){
@@ -35,6 +35,30 @@ preproc <- function(dat){
   undetrended_data<-cbind(data_downsampled,undetrended)
   
   return(undetrended_data)
+}
+
+
+baseline <- function(plot = FALSE){
+  # Making a (non-polynomial) spline to fit the baseline
+  sfunction2 <- splinefun(inflexion_points[o], inflexion_points_yval[o], method = "natural")
+  spline_base <- sfunction2(seq(1, length(undetrended_data$undetrended)), deriv = 0)
+  
+  # Plotting spline_base on spline_poly
+  if(plot){
+    plot(spline_poly)
+    points(inflexion_points[o], inflexion_points_yval[o], pch = 19)
+    lines(spline_base)
+  }
+  
+  # Correcting for baseline:
+  baseline_corrected <- undetrended_data$undetrended - spline_base
+  if(plot){
+    plot(baseline_corrected, type = "l")
+    # Plot new baseline (y = 0)
+    lines(1:length(undetrended_data$undetrended), seq(from = 0, to = 0, length.out = length(undetrended_data$undetrended)))
+  }
+  
+  return(baseline_corrected)
 }
 
 
