821307ef451dee95425e957e45c1149bdda1bd00
Johanna <jjf33@cam.ac.uk>
2020-07-02 09:30:20 +0100

    called all 'inflexion points' extrema

--- preproc.R
+++ preproc.R
@@ -41,13 +41,13 @@ preproc <- function(dat){
 
 baseline <- function(plot = FALSE){
   # Making a (non-polynomial) spline to fit the baseline
-  sfunction2 <- splinefun(inflexion_points[o], inflexion_points_yval[o], method = "natural")
+  sfunction2 <- splinefun(extrema[o], extrema_points_yval[o], method = "natural")
   spline_base <- sfunction2(seq(1, length(undetrended_data$undetrended)), deriv = 0)
   
   # Plotting spline_base on spline_poly
   if(plot){
     plot(spline_poly)
-    points(inflexion_points[o], inflexion_points_yval[o], pch = 19)
+    points(extrema[o], extrema_points_yval[o], pch = 19)
     lines(spline_base)
   }
   
