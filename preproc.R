f289ec15c36dfacbb889c03957bba11d184a1321
craig-powell-NT <craig.powell@ninjatheory.com>
2020-09-10 10:01:11 +0100

    Speed up downsampling of large files
    Repeated use of rbind to add rows gets slow as the number of rows increases, so we should try to reduce allocations

--- preproc.R
+++ preproc.R
@@ -1,6 +1,6 @@
 
 
-preproc <- function(dat){
+preproc <- function(data){
   dat<-data[!(data$PPG.PulseOx1=='NaN'),]
   
   #Downsample
@@ -11,6 +11,44 @@ preproc <- function(dat){
   
   list<-rle(dat$PPG.PulseOx1)
   ID <- rep(1:length(list$values), times = list$lengths)
+  data_downsampled <- c()
+  
+  nSrc <- nrow(dat)
+  iDst <- 1
+  iSrc <- 1
+  iVal <- 1
+  print("Deduplicating data...")
+  if(TRUE){ # Mode switch in case new method is worse or somehow broken
+  startClock <- Sys.time()
+  reportClock <- 10
+
+  data_downsampled <- cbind(dat, ID)
+
+  while (iSrc <= nSrc){
+    if (as.double(difftime(Sys.time(),startClock,unit="secs"))>reportClock){
+      min <- as.integer(reportClock/60)
+      sec <- reportClock %% 60
+      time <- paste("[",min,":",sep="")
+      if (sec == 0){ time <- paste(time,"00]",sep="")} else { time <- paste(time,sec,"]",sep="")}
+      print(paste(time,iSrc,"of",nSrc))
+      reportClock <- reportClock + 10
+    }
+
+    data_downsampled[iDst,] <- data_downsampled[iSrc,]
+    iDst <- iDst + 1
+    if (list$lengths[iVal] <= 4){
+      iSrc <- iSrc + list$lengths[iVal]
+      iVal <- iVal + 1
+    } else {
+      iSrc <- iSrc + 4
+    }
+  }
+
+  # Trim downsampled data to size
+  data_downsampled <- data_downsampled[1:(iDst-1),]
+
+  }else{ # Old method, using rbind which gets slow
+
   data2 <- cbind(dat, ID)
   data_downsampled <-c()
   
@@ -20,11 +58,13 @@ preproc <- function(dat){
       data_downsampled <- rbind(data_downsampled, sub.data[1,])
     }else if(nrow(sub.data) > 4 ){data_downsampled <- rbind(data_downsampled, sub.data[1,], sub.data[5,])}
   }
+
+  } # End of method selector
   
-  
+  print("Removing DC blocker...")
   #Undetrend
   # Analysis of device output indicates that the PPG signal is detrended by application of the following
-  # formula: OUT[i] = 80 + (OUT[i-1]-80) * 0.96875 + (IN[i] - [IN[i-1]), where the constance 0.96875 is
+  # formula: OUT[i] = 80 + (OUT[i-1]-80) * 0.96875 + (IN[i] - [IN[i-1]), where the constant 0.96875 is
   # an approximation fitted to the data.
   # Individual pulse events are more comprehensible if the detrending is not used, so this function 
   #removes it by inverting the above function. 
@@ -34,7 +74,7 @@ preproc <- function(dat){
     undetrended[i]<-((data_downsampled$PPG.PulseOx1[i]-80) - ((data_downsampled$PPG.PulseOx1[i-1]-80) * 0.96875) + (undetrended[i-1]))
   }
   undetrended_data<-cbind(data_downsampled,undetrended)
-  
+  print("Done")
   return(undetrended_data)
 }
 
